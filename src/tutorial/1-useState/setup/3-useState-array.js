e7990fa2f2c6ea9dd8e4a0c730a17f6bababc174
id3ntity99 <jas06113@gmail.com>
2021-09-27 16:41:42 +0900

    useState - Array

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  // useState sets the first item of the array to the argument in the paren.
+  const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  return (
+    <React.Fragment>
+      {people.map((person) => {
+        /*
+        const id = person.id;
+        const name = person.name;
+        Those two variables right above are same as below(const {id, name} = person;)
+        */
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>Remove</button>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        Clear All
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateArray;
+// If we invoke setPeople() directly, error pops out.
