40b7f2c8d513ad24360417a7a13c8c9735a1ef6a
id3ntity99 <jas06113@gmail.com>
2021-09-27 16:15:23 +0900

    useSate - Basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Title unchanged");
+  const handleClick = () => {
+    setText("Title Changed");
+  };
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
