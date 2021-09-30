64badf9818761bae4d49fa9bf6b403d292b8b3d5
id3ntity99 <jas06113@gmail.com>
2021-09-30 16:24:11 +0900

    useEffect - Basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,23 @@
-import React, { useState, useEffect } from 'react';
-// by default runs after every re-render
+import React, { useState, useEffect } from "react";
+// by default useEffect runs after every re-render
+// useEffect runs side-effect after rerender.
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("Call useEffect");
+    document.title = `New Messages(${value})`;
+  });
+  console.log("Components rerendered");
+  return (
+    <React.Fragment>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click me
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectBasics;
