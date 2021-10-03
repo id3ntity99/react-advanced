ffb96a0b06aa17c0d51c33c03a0d613622c06134
id3ntity99 <jas06113@gmail.com>
2021-10-03 15:16:12 +0900

    Ternary Operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,18 +3,22 @@ import React, { useState } from "react";
 // ternary operator
 
 const ShortCircuit = () => {
+  const [isError, setIsError] = useState(false);
   const [text, setText] = useState("");
-  const firstValue = text || "hello world";
-  // Wether text is empty or not, secondValue will be the second operand, "Hello world"
-  const secondValue = text && "hello world";
+  // const firstValue = text || "hello world";
+  // If first operand is truthy, then return second operand. If first operand is falsy, there is no return.
+  // const secondValue = text && "this is second value.";
 
   return (
     <React.Fragment>
       {/* <h1>{firstValue}</h1>
       <h1>value: {secondValue}</h1> */}
       <h1>{text || "john doe"}</h1>
-      {text && <h1>hello world</h1>}
-      {!text && <h1>hello world</h1>}
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? <p>An Error Occured</p> : <h1>There is no error, luckily.</h1>}
     </React.Fragment>
   );
 };
