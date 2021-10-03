9f2d137e9664f14ddf4a1404733316b8e5ff6a4b
id3ntity99 <jas06113@gmail.com>
2021-10-03 17:13:17 +0900

    Controlled Inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,12 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
 // React
 // value, onChange
+// without onChange, we cannot change the value of the input, because it's linked to useState, in this case.
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFristName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(firstName, email);
+  };
+
+  return (
+    <article>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="firstName">Name: </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={firstName}
+            placeholder="Your name"
+            onChange={(event) => setFristName(event.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email: </label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="abc123@example.com"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <button type="submit">Add person</button>
+      </form>
+    </article>
+  );
 };
 
 export default ControlledInputs;
