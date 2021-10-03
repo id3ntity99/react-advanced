a13d4e35b68d14c7f96dad44300faa8e7f5c6e4f
id3ntity99 <jas06113@gmail.com>
2021-10-03 13:37:48 +0900

    useEffect - Cleanup Function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    console.log("useEffect invoked");
+    window.addEventListener("resize", checkSize);
+    // This returning function is cleanup function.
+    // If this function doesn't exist, memory leak will be happened.
+    // return () => {
+    //   console.log("Cleanup");
+    //   window.removeEventListener("resize", checkSize);
+    // };
+    // Alternatively, we can use empty array as dependency.
+  }, []);
+
+  return (
+    <React.Fragment>
+      <h1>window</h1>
+      <h2>{size} px</h2>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectCleanup;
