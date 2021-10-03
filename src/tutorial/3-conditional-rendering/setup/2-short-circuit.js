38633a3cd379e2f9e18c0fce3810f898363b98a9
id3ntity99 <jas06113@gmail.com>
2021-10-03 15:02:08 +0900

    Short Circuit Evaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  // Wether text is empty or not, secondValue will be the second operand, "Hello world"
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <React.Fragment>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      <h1>{text || "john doe"}</h1>
+      {text && <h1>hello world</h1>}
+      {!text && <h1>hello world</h1>}
+    </React.Fragment>
+  );
 };
 
 export default ShortCircuit;
